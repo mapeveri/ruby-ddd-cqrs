f521719fbf7cad0bd0d04cc0d2016c22ea4ce2ca
Martín Peveri <martinpeveri@gmail.com>
2025-08-01 19:09:07 +0200

    Use to_h to map the values in the projection

--- src/chat/infrastructure/projection/add_message_projection_handler.rb
+++ src/chat/infrastructure/projection/add_message_projection_handler.rb
@@ -4,14 +4,7 @@ class Chat::Infrastructure::Projection::AddMessageProjectionHandler
 
     Chat::Infrastructure::Persistence::Redis::Projector::ChatMessagesProjector.store_message(
       chat_id: event.chat_id,
-      message: {
-        id: event.id,
-        send_id: event.sender_id,
-        receiver_id: event.receiver_id,
-        chat_id: event.chat_id,
-        content: event.content,
-        created_at: Time.now
-      }
+      message: event.to_h
     )
   end
 end
