a132118d131f94b1e7c7e08a9882d2890f8f951a
Martín Peveri <martinpeveri@gmail.com>
2025-08-01 17:12:49 +0200

    Rename test post_message_spec.rb

--- spec/requests/api/v1/message/post_message_spec.rb
+++ spec/requests/api/v1/message/post_message_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'Given a PostMessageController', type: :request do
+RSpec.describe 'Given a GetMessagesController', type: :request do
   describe 'POST /api/v1/message' do
     let(:id) { SecureRandom.uuid }
     let(:sender_id) { SecureRandom.uuid }
     let(:receiver_id) { SecureRandom.uuid }
     let(:chat_id) { SecureRandom.uuid }
     let(:content) { 'Hello, Bob!' }
-
 
     it 'creates a message and returns status created' do
       post '/api/v1/message', params: {
