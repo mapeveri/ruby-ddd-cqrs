7ed729c583277644fba1212083e4d4dfd8c0885b
Martín Peveri <martinpeveri@gmail.com>
2025-08-04 16:04:36 +0200

    Get or create user in join controller

--- app/controllers/api/v1/users/post_join_controller.rb
+++ app/controllers/api/v1/users/post_join_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::Users::PostJoinController < ApplicationController
   def call
-    token = SecureRandom.hex(16)
+    user_id = params[:user_id]
+    session_data = $redis.get("user:#{user_id}")
+    if session_data
+      Rails.logger.info("Getting user #{user_id} from cache")
+      data = JSON.parse(session_data)
+      render json: data and return
+    end
+
     name = "Guest_#{rand(1000)}"
-    data = { token: token, name: name }
-    $redis.setex("session:#{token}", 3600, data.to_json)
+    data = { user_id: user_id, name: name }
+    Rails.logger.info("Setting user #{user_id}")
+    $redis.setex("user:#{user_id}", 3600, data.to_json)
 
     render json: data
   end
--- spec/requests/api/v1/users/post_join_spec.rb
+++ spec/requests/api/v1/users/post_join_spec.rb
@@ -1,14 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe "POST /api/v1/users/join", type: :request do
+  let(:user_id) { SecureRandom.uuid }
+
+  before do
+    $redis.del("user:#{user_id}")
+  end
+
   describe "Given a user wants to join a chat" do
-    it "returns a token and a name" do
-      post "/api/v1/users/join"
+    it "returns a user_id and a name" do
+      post "/api/v1/users/join", params: {
+        user_id: user_id
+      }
 
       expect(response).to have_http_status(:ok)
 
       json = JSON.parse(response.body)
-      expect(json).to have_key("token")
+      expect(json).to have_key("user_id")
+      expect(json).to have_key("name")
+      expect(json["name"]).to match(/^Guest_/)
+    end
+  end
+
+  describe "Given a user is already log-in and want to join a chat" do
+    before do
+      data = { user_id: user_id, name: "Guest_#{rand(1000)}" }
+      $redis.set("user:#{user_id}", data.to_json)
+    end
+
+    after do
+      $redis.del("user:#{user_id}")
+    end
+
+    it "returns a user_id and a name from the cache" do
+      post "/api/v1/users/join", params: {
+        user_id: user_id
+      }
+
+      expect(response).to have_http_status(:ok)
+
+      json = JSON.parse(response.body)
+      expect(json).to have_key("user_id")
       expect(json).to have_key("name")
       expect(json["name"]).to match(/^Guest_/)
     end
