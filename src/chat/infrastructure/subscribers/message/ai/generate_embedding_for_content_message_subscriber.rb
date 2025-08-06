4e9c3bb54afd5b62fa47589c10e4cb67e5d58df5
Martín Peveri <martinpeveri@gmail.com>
2025-08-06 23:30:40 +0200

    Update projection after generating embedding in subscriber

--- src/chat/infrastructure/subscribers/message/ai/generate_embedding_for_content_message_subscriber.rb
+++ src/chat/infrastructure/subscribers/message/ai/generate_embedding_for_content_message_subscriber.rb
@@ -10,7 +10,16 @@ module Chat
     def call(event)
       Rails.logger.info("[GenerateEmbeddingForContentMessageSubscriber] -> Generating embedding")
       embedding = GeminiEmbeddingClient.embed_text(event.content)
+
       @active_record_embedding_writer.update_embedding(event.id, embedding)
+
+      Chat::Infrastructure::Persistence::Redis::Projector::ChatMessagesProjector.store_message(
+        chat_id: event.chat_id,
+        message: {
+          id: event.id,
+          embedding: embedding
+        }
+      )
     end
   end
 end
