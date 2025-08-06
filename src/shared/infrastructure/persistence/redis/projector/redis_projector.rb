27a1d886508ef90a97fc09cfe8b0ba0d683d62a3
Martín Peveri <martinpeveri@gmail.com>
2025-08-06 23:39:11 +0200

    Add optimistic locking to store method in redis projector

--- src/shared/infrastructure/persistence/redis/projector/redis_projector.rb
+++ src/shared/infrastructure/persistence/redis/projector/redis_projector.rb
@@ -3,19 +3,28 @@ class Shared::Infrastructure::Persistence::Redis::Projector::RedisProjector
     def store(key:, value:, index_field:)
       index_key = "#{key}:index"
       field_value = value[index_field] || value[index_field.to_sym]
-      position_index = $redis.hget(index_key, field_value)&.to_i
 
-      if position_index
-        Rails.logger.info("[RedisProjector] -> Updating #{key}")
-        current_value = JSON.parse($redis.lindex(key, position_index))
-        updated_value = safe_merge(current_value, value)
-        $redis.lset(key, position_index, updated_value.to_json)
-        return
+      $redis.watch(index_key, key) do
+        position_index = $redis.hget(index_key, field_value)&.to_i
+
+        result = $redis.multi do |multi|
+          if position_index
+            Rails.logger.info("[RedisProjector] -> Updating #{key}")
+            current_value = JSON.parse($redis.lindex(key, position_index))
+            updated_value = safe_merge(current_value, value)
+            multi.lset(key, position_index, updated_value.to_json)
+          else
+            Rails.logger.info("[RedisProjector] -> Inserting #{key}")
+            list_length = $redis.llen(key)
+            multi.rpush(key, value.to_json)
+            multi.hset(index_key, field_value, list_length)
+          end
+        end
+
+        unless result
+          raise "[RedisProjector] -> Transaction failed due to concurrent modification"
+        end
       end
-
-      Rails.logger.info("[RedisProjector] -> Inserting #{key}")
-      position_index = $redis.rpush(key, value.to_json).to_i - 1
-      $redis.hset(index_key, field_value, position_index)
     end
 
     def fetch(key:, limit: 50)
