1539848082ba819f644980b2ade6fa1722b0bc71
Martín Peveri <martinpeveri@gmail.com>
2025-08-07 18:14:52 +0200

    Remove unused method in event bus interface

--- src/shared/domain/bus/event_bus.rb
+++ src/shared/domain/bus/event_bus.rb
@@ -1,8 +1,4 @@
 class Shared::Domain::Bus::EventBus
-  def subscribe(event_class, handler)
-    raise NotImplementedError, "#{self.class} must implement #subscribe"
-  end
-
   def publish(events)
     raise NotImplementedError, "#{self.class} must implement #publish"
   end
