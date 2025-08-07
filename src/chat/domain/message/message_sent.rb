c51a3ce30d1737da5e23dbe81a198cebc03529f7
Martín Peveri <martinpeveri@gmail.com>
2025-08-07 16:48:16 +0200

    Add methods to specify domain event name in domain_event class

--- src/chat/domain/message/message_sent.rb
+++ src/chat/domain/message/message_sent.rb
@@ -11,15 +11,32 @@ class Chat::Domain::Message::MessageSent < Shared::Domain::DomainEvent
     @created_at = created_at
   end
 
-  def self.from(message)
-    new(
-      id: message.id.to_s,
-      sender_id: message.sender_id.to_s,
-      receiver_id: message.receiver_id.to_s,
-      content: message.content.to_s,
-      chat_id: message.chat_id.to_s,
-      created_at: message.created_at
-    )
+  class << self
+    def from(message)
+      new(
+        id: message.id.to_s,
+        sender_id: message.sender_id.to_s,
+        receiver_id: message.receiver_id.to_s,
+        content: message.content.to_s,
+        chat_id: message.chat_id.to_s,
+        created_at: message.created_at
+      )
+    end
+
+    def from_h(hash)
+      new(
+        id: hash["id"],
+        sender_id: hash["sender_id"],
+        receiver_id: hash["receiver_id"],
+        content: hash["content"],
+        chat_id: hash["chat_id"],
+        created_at: hash["created_at"]
+      )
+    end
+
+    def event_name
+      "chat.message.sent"
+    end
   end
 
   def to_h
--- src/shared/domain/domain_event.rb
+++ src/shared/domain/domain_event.rb
@@ -4,4 +4,22 @@ class Shared::Domain::DomainEvent
   def initialize
     @occurred_on = Time.now
   end
+
+  class << self
+    def from
+      raise "Not Implemented"
+    end
+
+    def from_h
+      raise "Not Implemented"
+    end
+
+    def event_name
+      raise "Not Implemented"
+    end
+  end
+
+  def to_h
+    raise "Not Implemented"
+  end
 end
