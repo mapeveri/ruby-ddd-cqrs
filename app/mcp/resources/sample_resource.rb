7b0db1e1b4e8ae07f2b5fb30e57f11ad963d6638
Martín Peveri <martinpeveri@gmail.com>
2025-09-20 15:08:27 +0200

    Initialize configuration for MCP server #20

---
+++ app/mcp/resources/sample_resource.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module MCP
+  class SampleResource < ApplicationResource
+    uri "examples/users"
+    resource_name "Users"
+    description "A user resource for demonstration"
+    mime_type "application/json"
+
+    def content
+      JSON.generate(User.all.as_json)
+    end
+  end
+end
---
+++ app/mcp/tools/application_tool.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module MCP
+  class ApplicationTool < ActionTool::Base
+    # write your custom logic to be shared across all tools here
+  end
+end
+
