90498cb199e192c3ffb8ce751bd4681897532007
Martín Peveri <martinpeveri@gmail.com>
2025-09-28 21:15:39 +0200

    Add ChatResource

---
+++ app/mcp/resources/chat_resource.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module MCP
+  class ChatResource < ApplicationResource
+    uri "examples/chats/messages"
+    resource_name "ChatMessages"
+    description "Messages belonging to a specific chat"
+    mime_type "application/json"
+
+    def initialize(**kwargs)
+      @query_bus = Container[:query_bus]
+      super()
+    end
+
+    def content(chat_id:)
+      query = Chat::Application::Message::Queries::GetMessagesQuery.new(
+        chat_id: chat_id
+      )
+
+      result = @query_bus.ask(query)
+
+      JSON.generate(result.as_json)
+    end
+  end
+end
