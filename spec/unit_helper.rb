59cec3b2928491246c2fd9aa2212d86ccf75519f
Martín Peveri <martinpeveri@gmail.com>
2026-08-09 13:16:56 +0200

    chore: upgrade Ruby to 3.4.10 and unify spec autoloading
    - Ruby 3.4.10 is required by Rails 8.1.2 (anonymous rest forwarding in blocks)
    - unit_helper now boots Rails so manual Zeitwerk loader no longer conflicts
      with Rails autoloader when unit and request specs run in the same process

--- spec/unit_helper.rb
+++ spec/unit_helper.rb
@@ -1,17 +1,1 @@
-require 'spec_helper'
-require 'zeitwerk'
-require 'faker'
-require 'uuidtools'
-require 'action_controller'
-
-loader = Zeitwerk::Loader.new
-loader.push_dir("#{__dir__}/../src")
-loader.push_dir("#{__dir__}/../app/controllers")
-loader.setup
-
-RSpec.configure do |config|
-  Dir[File.join(__dir__, 'unit/**/*.rb')]
-    .reject { |f| f =~ /_spec\.rb$/ }
-    .sort
-    .each { |f| require f }
-end
+require 'rails_helper'
